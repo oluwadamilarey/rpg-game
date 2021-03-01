fddf7ac6ea42fa05e2ce068e3ac8790c292a4aa2
Abimbola David <abimbola301@gmail.com>
2021-03-01 22:11:45 +0100

    implementing service route controller.

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -10,31 +10,25 @@ namespace rpg_game.Controllers
     [Route("[controller]")]
     public class CharacterController: ControllerBase
     {
-        private static List<Character> characters = new List<Character>{ // Defining List of Characters.
-            new Character(),
-            new Character { id = 1, Name = "Juwon"}
-        }; 
-
         private readOnly ICharacterService _characterService;// raedonly means it can only be assigned in the contructor.
 
-        public CharacterController(ICharacterService chracterService){ //this is class constructor
-
+        public CharacterController(ICharacterService characterService){ //this is class constructor
+            _characterService = characterService;
         }
 
         [Route("GetAll")]
         public IActionResult Get(){ // get request for returning a list of characters
-            return Ok(characters); // Remember Ok method refers to defining the status code for operation which in this case case is status 200 for success
+            return Ok(_characterService.GetAllCharacters()); // Remember Ok method refers to defining the status code for operation which in this case case is status 200 for success
         }
 
         [HttpGet("{id}")] // parameter for routing, the parameter for the route has to match the nname for the fuction parameter.
         public IActionResult GetSingle(int id){
-            return Ok(characters.FirstOrDefault(c => c.id == id));
+            return Ok(_characters.GetCharacterById());
         }
         
         [HttpPost]
         public IActionResult AddCharacter(Character newCharacter){
-            characters.Add(newCharacter);
-            return Ok(characters);
+            return Ok(_characters.AddNewCharacter(newCharacter));
         }
     }
 }
